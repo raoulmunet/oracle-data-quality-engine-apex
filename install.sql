dd74fbbdbe40694b23b310f08b3377993c9c24d6
raoulmunet <raoulmunet@gmail.com>
2026-09-23 19:21:33 +0300

    Add install.sql

---
+++ install.sql
@@ -0,0 +1,11 @@
+prompt Installing Oracle Data Quality Engine...
+
+@sql/01_tables.sql
+@sql/02_indexes.sql
+@sql/03_demo_source.sql
+@sql/04_demo_rules.sql
+@sql/05_reporting_views.sql
+@plsql/pkg_dq_engine.pks
+@plsql/pkg_dq_engine.pkb
+
+prompt Installation complete.
